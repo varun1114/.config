5d56ed32cc23da9a377012e37e98866f2f0a520e
Varun <varun@Varuns-MacBook-Pro.local>
2018-05-24 00:41:27 -0400

    Fix bash profile formatting and add script to link bash, gitconfig and psqlrc

---
+++ link.sh
@@ -0,0 +1,7 @@
+# Install nerdtree using pathogen
+ln -sf ~/Documents/dotfiles/.bash_profile ~/.bash_profile
+
+ln -sf ~/Documents/dotfiles/.gitconfig ~/.gitconfig
+
+ln -sf ~/Documents/dotfiles/.psqlrc ~/.psqlrc
+
